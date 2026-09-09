ren-yamanashi/awscdk-lint

fix: report grant* calls on optional Construct receivers in prefer-grants-property
An optional receiver such as `props.topic?.grantPublish()` is typed `Topic | undefined`,
which has no symbol, so isConstructType rejected it and the rule never looked at the call.
Strip the nullish members of the receiver type before the Construct check, which also gives
the rule the member type it needs to resolve the `grants` property.

packages/oxlint/src/rules/prefer-grants-property.ts
@@ -1,6 +1,7 @@
 import { AST_NODE_TYPES, ESLintUtils } from "corsa-oxlint";
 
 import { isConstructType } from "../core/cdk-construct/type-checker/is-construct";
+import { findNonNullableType } from "../core/ts-type/finder/non-nullable-type";
 import { createRule } from "../shared/create-rule";
 
 export const preferGrantsProperty = createRule({
@@ -35,8 +36,13 @@ export const preferGrantsProperty = createRule({
         if (!methodName.startsWith("grant")) return;
 
         const objectNode = node.callee.object;
-        const type = parserServices.getTypeAtLocation(objectNode);
-        if (!type || !isConstructType(type, checker)) return;
+        const receiverType = parserServices.getTypeAtLocation(objectNode);
+        if (!receiverType) return;
+
+        // NOTE: An optional receiver such as `props.topic?.grantPublish()` is typed
+        // `Topic | undefined`, so strip the nullish members before inspecting the Construct.
+        const type = findNonNullableType(receiverType, checker);
+        if (!isConstructType(type, checker)) return;
 
         const grantsProperty = checker.getPropertiesOfType(type).find((s) => s.name === "grants");
         if (!grantsProperty) return;

ren-yamanashi/eslint-cdk-plugin

packages/oxlint/src/rules/prefer-grants-property.ts
@@ -1,6 +1,7 @@
 import { AST_NODE_TYPES, ESLintUtils } from "corsa-oxlint";
 
 import { isConstructTypeIgnoringSubclasses } from "../core/cdk-construct/type-checker/is-construct";
+import { findNonNullableType } from "../core/ts-type/finder/non-nullable-type";
 import { createRule } from "../shared/create-rule";
 
 export const preferGrantsProperty = createRule({
@@ -35,8 +36,13 @@ export const preferGrantsProperty = createRule({
         if (!methodName.startsWith("grant")) return;
 
         const objectNode = node.callee.object;
-        const type = parserServices.getTypeAtLocation(objectNode);
-        if (!type || !isConstructTypeIgnoringSubclasses(type, checker)) return;
+        const receiverType = parserServices.getTypeAtLocation(objectNode);
+        if (!receiverType) return;
+
+        // NOTE: An optional receiver such as `props.topic?.grantPublish()` is typed
+        // `Topic | undefined`, so strip the nullish members before inspecting the Construct.
+        const type = findNonNullableType(receiverType, checker);
+        if (!isConstructTypeIgnoringSubclasses(type, checker)) return;
 
         const grantsProperty = checker.getPropertiesOfType(type).find((s) => s.name === "grants");
         if (!grantsProperty) return;